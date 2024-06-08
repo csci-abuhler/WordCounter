9b2f9cbc07e969001a722122493f83fefc134636
Andrew Buhler <terminalvelocity13@gmail.com>
2024-06-08 19:31:30 -0400

    Update 2
    - Word counter now counts words in a file. Next part is to build a GUI.

--- src/counter/WordCounter.java
+++ src/counter/WordCounter.java
@@ -1,11 +1,40 @@
 package counter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordCounter {
 	public static void main(String[] args) {
-		
+		WordCounter wc = new WordCounter();
+		wc.execute();
 	} // main
-	
+
+	// Executes the main point of the program.
+	private void execute() {
+		// Try catch in case errors occur.
+		try {
+			// Initialize file, scanner, and counter
+			File f = new File("E:\\Eclipse\\WordCounter\\src\\test files\\test.txt");
+			Scanner myReader = new Scanner(f);
+			int count = 0;
+			
+			// Count the words
+			while (myReader.hasNext()) {
+				myReader.next();
+				count++;
+			} // while
+			
+			myReader.close();
+			System.out.println(count);
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		} // try catch
+	} // execute
+
+	// constructor
 	public WordCounter() {
-		
+
 	} // constructor
 } // class
