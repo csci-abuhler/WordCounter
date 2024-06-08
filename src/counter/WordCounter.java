f03c63564a8a89c0155c1d0e61453c0abfa1f5b7
Andrew Buhler <terminalvelocity13@gmail.com>
2024-06-08 17:40:26 -0400

    Initial commit

---
+++ src/counter/WordCounter.java
@@ -0,0 +1,11 @@
+package counter;
+
+public class WordCounter {
+	public static void main(String[] args) {
+		
+	} // main
+	
+	public WordCounter() {
+		
+	} // constructor
+} // class
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author termi
+ *
+ */
+module WordCounter {
+}
